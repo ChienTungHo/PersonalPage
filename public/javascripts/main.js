c43df6b804ef12816497209994b35220b679ad11
ChienTungHo <ho_tongtong@yahoo.com.tw>
2013-11-11 05:57:15 -0500

    "Launch the webpage to digital ocean"

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-				
-		$('.modal-btn').on('click',function(){
+		$('.modal-btn').on('click',function(e){
 			$('#'+$(this).data('modal')).modal('toggle');
+			$('#'+$(this).data('modal')).modal().css({
+                		'top': function () {
+					console.log(window.pageYOffset);
+                    			return window.pageYOffset;
+                		},
+				'margin-top': '30px',
+            		});
+			//console.log($('#'+$(this).data('modal')).modal());
 			$('.modal-backdrop').removeClass('modal-backdrop');
 		});
 
 		$('a[href^="#"]').on('click',function (e) {
 			e.preventDefault();
-
 			var target = this.hash,
 			$target = $(target);
 			$('html, body').stop().animate({
@@ -15,5 +21,7 @@ $(document).ready(function(){
 				}, 900, 'swing', function () {
 				window.location.hash = target;
 				});
-			});
+
+		});
+
 });
